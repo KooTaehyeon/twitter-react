3e392f3420e74778bb9613f190b017d48b4831c8
Kth <vcro12123@naver.com>
2022-04-22 19:12:34 +0900

    프로필 기능 설정

--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,13 +1,27 @@
 import React from 'react';
+import { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { auth } from '../fbase';
-
-const Profile = () => {
+import { auth, dbService } from '../fbase';
+import { collection, getDocs, query, where } from '@firebase/firestore';
+const Profile = ({ userObj }) => {
   const history = useNavigate();
   const onLogOut = () => {
     auth.signOut();
     history(-1);
   };
+  const getMyNweets = async () => {
+    const q = query(
+      collection(dbService, 'wteers'),
+      where('creatorId', '==', userObj.uid)
+    );
+    const querySnapshot = await getDocs(q);
+    querySnapshot.forEach((doc) => {
+      console.log(doc.id, ' => ', doc.data());
+    });
+  };
+  useEffect(() => {
+    getMyNweets();
+  }, []);
   return (
     <div>
       <button onClick={onLogOut}>log out</button>
